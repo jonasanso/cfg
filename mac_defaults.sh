bef6877d22c4ab10d2763496641428cd84428443
jonas Anso <jonas.anso@gmail.com>
2020-06-19 15:26:19 +0100

    add mac defaults

---
+++ mac_defaults.sh
@@ -0,0 +1,3 @@
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
+defaults write com.apple.finder QuitMenuItem -bool true; killall Finder
+defaults write com.apple.finder AppleShowAllFiles -bool true; killall Finder
